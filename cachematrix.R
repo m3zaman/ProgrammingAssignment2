668c0cb09f2abda3ab73c4eb6b8965993fc4abd5
MM Mamunuzzaman <mamun@MMs-MacBook-Pro.local>
2014-12-21 15:42:05 +0600

    Completed the r functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix receives a matrix as an input
+## and returns a special list that stores a matrix
+## and cache's its inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  I <- NULL
+  set <- function(y) {
+    x <<- y
+    I <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) I <<- inv
+  getinv <- function() I
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
+## checks if the matrix is the same then
+## returns the cached copy
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## i is the inverse matrix of the one passed as input, x
+  i <- x$getinv()
+  ## o is the original matrix extracted from x
+  o <- x$get()
+  
+  ## if A and AI (inverse of A) are 2 matrixes
+  ## then A %*% AI == AI %*% A
+  ## we can use that logic to identify that the matrix o
+  ## has the correct inverse in i
+  
+  if(!is.null(i) && (o %*% i == i %*% o)) {
+    message("getting cached data")
+    return(i)
+  }
+  
+  ## we do not have any cached copy, need to calculate one
+  ## assumptions are o is a square and invertible matrix
+  calcinv <- solve(o)
+  x$setinv(calcinv)
+  calcinv
 }
